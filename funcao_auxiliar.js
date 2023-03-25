e144a704d1a70e5b2418fb073247a33ecf4c9769
DeniseBarbosa <deniseluvoir@gmail.com>
2023-03-25 16:41:21 -0300

    Praticando os Conceitos de Importação e Exportação

---
+++ funcao_auxiliar.js
@@ -0,0 +1,14 @@
+const entradas = [10, 5, 50, 10, 98, 400, 401, 450, 800, 801, 23, 33, 44, 100, 1, 7];
+let i = 0;
+
+function gets() {
+    const valor = entradas[i];
+    i = i + 1;
+    return valor;
+}
+
+function print(texto) {
+    console.log(texto);
+}
+
+module.exports = { gets, print };
---
+++ main.js
@@ -0,0 +1,31 @@
+/**
+ * Uma sala contem 5 alunos e para cada aluno foi sorteado um número de 1 -100.
+ * faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado
+ * Dados de entrada:
+ * 5
+ * 50
+ * 10
+ * 98
+ * 23
+ * saída:
+ * 98
+ * 
+ */
+
+const { gets, print } = require('./funcao_auxiliar');
+
+const quantidadeDeAlunos = gets();
+let maiorValorEncontrado = 0;
+
+
+for (let i = 0; i < quantidadeDeAlunos; i++) {
+    const numerosSorteado = gets();
+    if (numerosSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numerosSorteado;
+    }
+
+}
+
+
+
+print(maiorValorEncontrado);
