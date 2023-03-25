48ffe7524aafe09916153af680b6c78f96aa4ac1
DeniseBarbosa <deniseluvoir@gmail.com>
2023-03-25 13:48:43 -0300

    Praticando Estruturas de Repetição

---
+++ arrays_repeticao.js
@@ -0,0 +1,25 @@
+/**
+ * Crie um programa que dado um número imprima a sua tabuada
+ * Crie um programa que seja capz de percorrer uma lista de numeros e imprima cada numero Par encontrado
+ * 
+ */
+
+const numero = 7;
+for (let i = 1; i < 10; i++) {
+
+   // console.log(i * numero);
+
+}
+
+const numeros = [8,25,14,1,2,64,73,26];
+
+
+for (let i = 0; i < numeros.length; i++) {
+    const lista = numeros[i];
+    if (lista % 2 === 0) {
+        console.log(lista);
+    }
+    
+    
+}
+
